7c9a6b43bf819f9c1e4d8053e51e3a971b42a24e
Sabrimrn <sabri.merroun@gmail.com>
2025-06-01 18:08:47 +0200

    Initial commit

---
+++ src/test/java/org/example/sabrimerroun/SabriMerrounApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sabrimerroun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabriMerrounApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
